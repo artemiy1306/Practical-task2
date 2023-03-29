92521e48b18bee0a9bfa11457bc1d7bdff3a841d
artemiy <karelskiy@mail.ru>
2023-03-29 21:23:59 +0300

    zad15

---
+++ zad15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите порядковый номер дня недели: ");
+string number = Console.ReadLine();
+switch (Convert.ToInt32(number)){
+case 1:
+Console.WriteLine("нет"); break;
+case 2:
+Console.WriteLine("нет"); break;
+case 3:
+Console.WriteLine("нет"); break;
+case 4:
+Console.WriteLine("нет"); break;
+case 5:
+Console.WriteLine("нет"); break;
+case 6:
+Console.WriteLine("да"); break;
+case 7:
+Console.WriteLine("да"); break;
+}
