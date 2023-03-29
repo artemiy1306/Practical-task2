d40cc2a82d33bf8edcc8831e283398376f484f52
artemiy <karelskiy@mail.ru>
2023-03-29 21:22:41 +0300

    add zad10

---
+++ zad10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+  //и на выходе показывает вторую цифру этого числа.
+Console.Clear(); 
+Console.Write("Введите число: "); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+int result = 0;
+number = number / 10;
+result = number % 10;
+Console.WriteLine($"второе число = {result}");
